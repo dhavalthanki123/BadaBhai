4571a2b36ef365effb8230442ddddf54955c3f33
dhavalthanki123 <31736509+dhavalthanki123@users.noreply.github.com>
2019-05-02 20:15:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dhava/badabhai/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dhava.badabhai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
